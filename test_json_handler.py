e067cd41f1c378d627c4462bfb5300b87caf690a
attal23 <159833100+attal23@users.noreply.github.com>
2024-07-02 23:00:31 -0400

    Update test_json_handler.py

--- test_json_handler.py
+++ test_json_handler.py
@@ -6,7 +6,7 @@ from json_handler import JsonHandler
 
 
 @pytest.fixture
-def json_handler():
+def json_handler() -> JsonHandler:
     """Provides an instance of JsonHandler for tests."""
     return JsonHandler()
 
